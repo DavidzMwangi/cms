440a275cc48c6dc30a3a85e415f2f9a05d0aa62f
Sekina-Muntaz <sherzmuntaz@gmail.com>
2018-07-03 15:20:50 +0300

    resolve

---
+++ admin/index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include_once '../login/user.php';
+$user = new User;
+$id = $_SESSION['id'];
+if (!$user->session()){
+    header("location:../login.php");
+}
+else{
+    if (!$user->isAdmin()){
+        header("location:../index.php");
+    }
+}
+if (isset($_REQUEST['q'])){
+    $user->logout();
+    header("location:login.php");
+}
+?>
+<html>
+
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../assets/css/style.css" />
+</head>
+
+<body>
+<div class="form">
+    <h1>Welcome <?php $user->fullname($id);?></h1>
+    <p align="right"><a href="?q=logout">LOGOUT</a></p>
+</div>
+</body>
+
+</html>
