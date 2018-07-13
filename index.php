d39ad0dddc8f08d16d0c5a5e86ce88a152ae7151
efkei <gichinirahab2@gmail.com>
2018-07-13 14:19:06 +0300

    Thats all

--- index.php
+++ index.php
@@ -2,8 +2,21 @@
 <?php
 include_once 'header.php';
 ?>
-<div class="container">
+<div class="container" >
+  <nav class="navbar">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="index.php" >Refresh page</a>
+      </div>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Sign in</a></li>
+      </ul>
+    </div>
+  </nav>
 
+  <div class="project-name">
+    <h1>ANIMAL MANAGEMENT SYSTEM</h1>
+  </div>
 </div>
 <?php
 include_once 'footer.php';
