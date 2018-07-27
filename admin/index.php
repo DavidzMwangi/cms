06c62954aea9035a6708fc322f0f517266c90c73
samumo.dev@gmail.com <samumo.dev@gmail.com>
2018-07-27 13:45:28 +0300

    sdfghdfsdr

--- admin/index.php
+++ admin/index.php
@@ -60,52 +60,10 @@
 </head>
 <body>
 <div class="wrapper">
-    <!-- sidebar -->
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <img src="../assets/images/profile.jpeg" class="rounded-circle " height="100px;" width="100px">
-            <div class="d-block" style="color: white; padding-left: 30px">Admin </div>
-        </div>
 
-        <ul class="list-styled components">
-            <p>Menu </p>
-            <!-- <li class="active">
-                    <a href="#homesubmenu" data-toggle="collapse" aria-expanded="false"  class="dropdown-toggle">Home</a>
-                    <ul class="collapse list-styled" id="homesubmenu">
-                      <li><a href="">Home 1</a></li>
-                      <li><a href="">Home 2</a></li>
-                      <li><a href="">Home 3</a></li>
-                    </ul>
-                </li> -->
-
-            <li><a href=""><i class="fa fa-address-card mr-3"></i>Dashboard</a></li>
-
-            <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">Records</a>
-                <ul class="collapse show list-styled" id="pageSubmenu">
-                    <li>
-                        <a href="#" onclick="loadTodaysData(event)" > Daily</a>
-                    </li>
-                    <li>
-                        <a href="#" onclick="loadMonthlyData(event)">Monthly</a>
-                    </li>
-                    <li>
-                        <a href="#" onclick="loadMonthlyAverageData(event)">Monthly Average For Cow</a>
-                    </li>
-                    <li>
-                        <a href="#" onclick="loadYearlyData(event)">Year</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Portfolio</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-    </nav>
-
+    <?php
+    include_once 'sidebar.php';
+    ?>
     <!-- page content -->
     <div id="content" class="w-100 ml-2">
 
--- admin/monthaverage.php
+++ admin/monthaverage.php
@@ -10,5 +10,10 @@ if (isset($_GET['q'])){
 
 $sql ="select avg(morning) as morning, avg(evening) as evening, cow_id 
 from milk_records where MONTH(date) = '".$month ."' group by cow_id";
+
+
+
 $result = $conn->query($sql);
+
+$data = array_merge($result->fetchAll(),$conn->query($sql2)->fetchAll());
 echo json_encode($result->fetchAll(), false);
