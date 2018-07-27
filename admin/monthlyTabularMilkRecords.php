6d09cf98eecbd73ae8cb8ae4158e46f95bc3b1b2
DavidzMwangi <mwangidavidwanjohi@gmail.com>
2018-07-27 11:42:18 +0300

    completed mothly table record

--- admin/monthlyTabularMilkRecords.php
+++ admin/monthlyTabularMilkRecords.php
@@ -31,6 +31,8 @@ $milk_record=new MilkRecord();
     <link rel="stylesheet" href="../assets/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css"  href="../assets/css/c3.min.css" />
     <link rel="stylesheet" type="text/css"  href="../assets/css/sidebar.css" />
+    <link rel="stylesheet" type="text/css" href="../assets/plugins/DataTables/datatables.css">
+
     <style>
         .months{
             border: 1px solid rgba(0, 0, 0, 0.125);
@@ -89,37 +91,44 @@ $milk_record=new MilkRecord();
 
                 <div class="card-body">
 
-
+                    <?php
+//                    echo date('F')
+                    ?>
                     <table id="table_id" class="display">
                         <thead>
                         <tr>
                             <th>Month</th>
                             <th>Total No of Herds</th>
                             <th>Total Milk Amount(Litres)</th>
-                            <th>Average Amount per Day(litres)</th>
-                            <th>Herd Average per Day</th>
+                            <th>Average Morning Amount (litres)</th>
+                            <th>Average Evening Amount (litres)</th>
                             <th>Herd Average per Month</th>
                         </tr>
                         </thead>
                         <tbody>
 
 
-                        <tr>
-                            <td>January</td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-<!--                        --><?php
-//
-//                        foreach ($milk_record->monthlyRecords() as $a){
-//                            echo $a['id'] . '</br>' ;
-//
-//                        }
 
-//                        ?>
+                    <?php
+//                    echo $milk_record->monRec(1);
+//                    echo $milk_record->monRec(2);
+//                    echo $milk_record->monRec(3);
+//                    echo $milk_record->monRec(4);
+//                    echo $milk_record->monRec(5);
+//                    echo $milk_record->monRec(6);
+//                    echo $milk_record->monRec(7);
+//                    echo $milk_record->monRec(8);
+//                    echo $milk_record->monRec(9);
+//                    echo $milk_record->monRec(10);
+//                    echo $milk_record->monRec(11);
+//                    echo $milk_record->monRec(12);
+
+                    for ($i=1;$i<=12;$i++){
+                        echo $milk_record->monRec($i);
+                    }
+
+
+                    ?>
 
                         </tbody>
                     </table>
@@ -143,8 +152,18 @@ $milk_record=new MilkRecord();
 <script src="../assets/js/c3.min.js"></script>
 <script src="../assets/js/main.js"></script>
 <script src="js/charts.js"></script>
+<script type="text/javascript" charset="utf8" src="../assets/plugins/DataTables/datatables.js"></script>
 
 <script>
+
+    $(document).ready(function () {
+        $('#table_id').DataTable({
+            "ordering": false,
+            "pageLength":12
+        });
+
+
+    });
 
 </script>
 </body>
